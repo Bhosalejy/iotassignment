676ca3473ebcaba987a9685c03329b4f909add28
E1_Jyoti_83865 <jsbhosale.com>
2024-04-16 22:36:30 +0530

    iotassignment

---
+++ assignment1/A1Q4.py
@@ -0,0 +1,19 @@
+# maximum of three numbers
+
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+num3 = int(input("Enter num3 : "))
+
+if num1 > num2:
+    print("num1 is greater")
+elif num2 > num3:
+    print("num2 is greater")   
+else:
+    print("num3 is greater")   
+
+
+
+
+
+
+
