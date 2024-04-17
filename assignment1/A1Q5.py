7abacb2fd222519e6a6bee4340c4cdbe8d7e6f01
E1_Jyoti_83865 <jsbhosale.com>
2024-04-17 21:18:24 +0530

    iotassignment

---
+++ assignment1/A1Q5.py
@@ -0,0 +1,27 @@
+
+
+sub1 = float(input("Enter the sub1 : "))
+sub2 = float(input("Enter the sub2 : "))
+sub3 = float(input("Enter the sub3 : "))
+
+
+average = (sub1 + sub2 + sub3) / 3
+
+
+print("The average of the 3 sub is:", average)
+
+if average > 90 :
+    print ("grade A")
+elif average > 80 :
+    print ("grade B")
+elif average > 70 :
+    print ("grade C")   
+elif average > 60 :
+    print ("grade D")
+else:
+    print ("grade F")
+
+
+
+
+
